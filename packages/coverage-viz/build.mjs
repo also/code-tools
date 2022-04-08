dd2688b9c00440af3a1377493d682461aabd2a92
Ryan Berdeen <ryan@ryanberdeen.com>
2022-04-07 20:48:45 -0400

    build and serve options

--- packages/coverage-viz/build.mjs
+++ packages/coverage-viz/build.mjs
@@ -1,14 +1,23 @@
-import { build } from "esbuild";
+import { serve, build } from "esbuild";
 
-build({
+const opts = {
   logLevel: "info",
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   bundle: true,
   sourcemap: true,
   minify: true,
-  watch: true,
   loader: {
     ".ttf": "file",
   },
-});
+};
+
+const mode = process.argv[2] || "build";
+
+if (mode === build) {
+  build(opts);
+} else if (mode === "watch") {
+  build({ ...opts, watch: true });
+} else if (mode === "serve") {
+  serve({ servedir: "." }, opts);
+}
