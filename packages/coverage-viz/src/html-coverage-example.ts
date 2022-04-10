f69a7f858e27b25373519fdc621322bcb555e544
Ryan Berdeen <ryan@ryanberdeen.com>
2022-04-10 18:13:53 -0400

    clean up html coverage example

--- packages/coverage-viz/src/html-coverage-example.ts
+++ packages/coverage-viz/src/html-coverage-example.ts
@@ -1,12 +1,13 @@
+import { ChromeBasicCoverage } from "@also/mapped-coverage/lib/types";
 import { coverageOnly } from "./generate.js";
 import { showEditor } from "./mapped-editor.js";
 
+const coverage: ChromeBasicCoverage =
+  require("../example/html-coverage-example.json")[0];
+
 async function run() {
   const start = Date.now();
-  const data = await coverageOnly(
-    require("../example/coverage-input.html.txt"),
-    require("../example/html-coverage-example.json")[0]
-  );
+  const data = await coverageOnly(coverage.text!, coverage);
   const end = Date.now();
   console.log(`Generated in ${end - start}ms`);
 
--- packages/mapped-coverage/src/types.ts
+++ packages/mapped-coverage/src/types.ts
@@ -18,4 +18,5 @@ export type ChromeBasicCoverage = {
     start: number;
     end: number;
   }[];
+  text?: string;
 };
