06459e896e7a5f9fd720656d94af50f8b6f97454
Ryan Berdeen <ryan@ryanberdeen.com>
2023-02-04 01:15:49 +0000

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,7 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
     "no-constant-condition": ["error", { checkLoops: false }],
